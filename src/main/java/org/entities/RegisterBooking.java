d3ff914096c4bdc13db079741c90edab2455c10e
Santiago <santiago.garciah17@gmail.com>
2024-12-23 13:13:04 -0500

    fix: id on RegisterBooking deleted

--- src/main/java/org/entities/RegisterBooking.java
+++ src/main/java/org/entities/RegisterBooking.java
@@ -4,18 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
-import java.util.UUID;
 
 @Getter
 @Setter
 public class RegisterBooking {
-    private String id;
     private Date startDate;
     private Date endDate;
 
     public RegisterBooking(Date startDate, Date endDate) {
         this.startDate = startDate;
         this.endDate = endDate;
-        this.id = UUID.randomUUID().toString();
     }
 }
