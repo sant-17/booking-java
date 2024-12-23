6aed2069e8dc6c1c3ea792997574897e247d378c
Santiago <santiago.garciah17@gmail.com>
2024-12-23 14:17:57 -0500

    feat: DayTrip get total price added

--- src/main/java/org/entities/DayTrip.java
+++ src/main/java/org/entities/DayTrip.java
@@ -13,15 +13,17 @@ public class DayTrip {
     private String id;
     private String name;
     private double rating;
-    private double price;
+    private double pricePerAdult;
+    private double pricePerKid;
     private List<Booking> bookings;
     private List<Amenity> amenities;
 
-    public DayTrip(String name, double rating, double price) {
+    public DayTrip(String name, double rating, double pricePerAdult, double pricePerKid) {
         this.id = UUID.randomUUID().toString();
         this.name = name;
         this.rating = rating;
-        this.price = price;
+        this.pricePerAdult = pricePerAdult;
+        this.pricePerKid = pricePerKid;
         this.bookings = new ArrayList<>();
         this.amenities = new ArrayList<>();
     }
@@ -32,5 +34,9 @@ public class DayTrip {
 
     public void addAmenity(Amenity amenity) {
         this.amenities.add(amenity);
+    }
+
+    public double getTotalPrice(Integer adults, Integer kids) {
+        return (adults * pricePerAdult) + (kids * pricePerKid);
     }
 }
