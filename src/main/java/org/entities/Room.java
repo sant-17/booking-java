498c396d44d5ee0cadd9e490a160d25dbf1ec2fd
Santiago <santiago.garciah17@gmail.com>
2024-12-23 13:14:02 -0500

    fix: Comment deleted

--- src/main/java/org/entities/Room.java
+++ src/main/java/org/entities/Room.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -13,14 +14,14 @@ public class Room {
     private Integer id;
     private String type;
     private String description;
-    private double price;
+    private double pricePerNight;
     private List<RegisterBooking> bookings;
 
-    public Room(Integer id, String type, String description, double price) {
+    public Room(Integer id, String type, String description, double pricePerNight) {
         this.id = id;
         this.type = type;
         this.description = description;
-        this.price = price;
+        this.pricePerNight = pricePerNight;
         this.bookings = new ArrayList<>();
     }
 
@@ -36,4 +37,54 @@ public class Room {
         }
         return true;
     }
+
+    public double getTotalToPay(Date startDate, Date endDate) {
+        Calendar startDateCal = Calendar.getInstance();
+        startDateCal.setTime(startDate);
+        Calendar endDateCal = Calendar.getInstance();
+        endDateCal.setTime(endDate);
+
+        int days = endDateCal.get(Calendar.DAY_OF_YEAR) - startDateCal.get(Calendar.DAY_OF_YEAR) + 1;
+
+        double subtotal = days * this.pricePerNight;
+
+        int increaseLast5Days = 15;
+        int increaseDays10to15 = 10;
+        int discountDays5to10 = -8;
+
+        int totalPercentage = 0;
+
+        Calendar currentDate = Calendar.getInstance();
+        currentDate.setTime(startDate);
+
+        boolean appliedIncreaseLast5Days = false;
+        boolean appliedIncreaseDays10to15 = false;
+        boolean appliedDiscountDays5to10 = false;
+
+        while (currentDate.before(endDateCal) || currentDate.equals(endDateCal)) {
+            int dayOfMonth = currentDate.get(Calendar.DAY_OF_MONTH);
+
+            if (dayOfMonth >= 26 && !appliedIncreaseLast5Days) {
+                totalPercentage += increaseLast5Days;
+                appliedIncreaseLast5Days = true;
+            }
+
+            else if (dayOfMonth >= 10 && dayOfMonth <= 15 && !appliedIncreaseDays10to15) {
+                totalPercentage += increaseDays10to15;
+                appliedIncreaseDays10to15 = true;
+            }
+
+            else if (dayOfMonth >= 5 && dayOfMonth <= 10 && !appliedDiscountDays5to10) {
+                totalPercentage += discountDays5to10;
+                appliedDiscountDays5to10 = true;
+            }
+
+            currentDate.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
+        double finalPrice = (subtotal * (totalPercentage + 100))/100;
+
+        return finalPrice;
+    }
+
 }
