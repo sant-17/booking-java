1172c3e69d5936737f1a2ca9d5ea6823f4c58405
Santiago <santiago.garciah17@gmail.com>
2024-12-24 02:22:11 -0500

    fix: lombok anotations for constructors added

--- src/main/java/org/entities/RegisterBooking.java
+++ src/main/java/org/entities/RegisterBooking.java
@@ -1,18 +1,17 @@
 package org.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterBooking {
     private Date startDate;
     private Date endDate;
-
-    public RegisterBooking(Date startDate, Date endDate) {
-        this.startDate = startDate;
-        this.endDate = endDate;
-    }
 }
