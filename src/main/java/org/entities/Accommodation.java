7f3cc0c5ca12a03c854ba3f42d3a898f33123560
Santiago <santiago.garciah17@gmail.com>
2024-12-24 02:26:38 -0500

    feat: getAvailableRooms method and ToString added

--- src/main/java/org/entities/Accommodation.java
+++ src/main/java/org/entities/Accommodation.java
@@ -1,14 +1,17 @@
 package org.entities;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class Accommodation {
     private String id;
     private String name;
@@ -32,5 +35,42 @@ public class Accommodation {
 
     public void addBooking(Booking booking) {
         this.bookings.add(booking);
+    }
+
+    public List<Room> getAvailableRooms(Date startDate, Date endDate) {
+        List<Room> availableRooms = new ArrayList<>();
+        for (Room room : this.rooms) {
+            if (room.isAvailable(startDate, endDate)) {
+                availableRooms.add(room);
+            }
+        }
+        return availableRooms;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Alojamiento [ID: ").append(id)
+                .append(", Nombre: ").append(name)
+                .append(", Tipo: ").append(type)
+                .append(", Calificación: ").append(rating)
+                .append("]\nHabitaciones:\n");
+        for (Room room : rooms) {
+            sb.append("- Habitación ID: ").append(room.getId())
+                    .append(", Tipo: ").append(room.getType())
+                    .append(", Descripción: ").append(room.getDescription())
+                    .append(", Precio/Noche: $").append(room.getPricePerNight())
+                    .append("\n");
+        }
+        sb.append("Reservas:\n");
+        for (Booking booking : bookings) {
+            sb.append("- Reserva para: ").append(booking.getClient().getFullName())
+                    .append(", Habitación: ").append(booking.getRoom().getId())
+                    .append(", Check in: ").append(booking.getStartDate())
+                    .append(", Check out: ").append(booking.getEndDate())
+                    .append(", Pago total: $").append(booking.getTotalPay())
+                    .append("\n");
+        }
+        return sb.toString();
     }
 }
